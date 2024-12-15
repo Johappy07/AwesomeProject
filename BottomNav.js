c3a317036cac3a82b99d2c0813e54e714f78a381
Johappy07 <johanesberchmann@gmail,com>
2024-12-15 13:20:09 +0700

    Update Baru

--- BottomNav.js
+++ BottomNav.js
@@ -2,31 +2,29 @@ import * as React from 'react';
 import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Profile from './App';
-import Mahasiswa from './Mahasiswa';
+import Home from './home';
+import List from './List';
+import Map from './Map';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import {faUser} from '@fortawesome/free-solid-svg-icons';
+import {faUser, faMap, faHouse, faUsers} from '@fortawesome/free-solid-svg-icons';
 import { faUserGraduate } from '@fortawesome/free-solid-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { WebView } from 'react-native-webview';
 function HomeScreen() {
   return (
-    <Profile />
+    <Home />
   );
 }
 
-function DataMahasiswaScreen() {
+function ListScreen() {
   return (
-    <Mahasiswa />
+    <List />
   );
 }
 
-function WebScreen() {
+function MapScreen() {
   return (
-    <WebView
-        source={{ uri: 'https://github.com/Johappy07' }}
-        
-      />
+    <Map/>
   );
 }
 
@@ -36,19 +34,19 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Profile" component={HomeScreen} 
+        <Tab.Screen name="Home" component={HomeScreen} 
       options={{ headerShown: false,
         tabBarIcon: ({ color }) => (
-          <FontAwesomeIcon icon={faUser} color={color} size={20} />
+          <FontAwesomeIcon icon={faHouse} color={color} size={20} />
         ),}}/>
-        <Tab.Screen name="Mahasiswa" component={DataMahasiswaScreen}  options={{ headerShown: true,
+        <Tab.Screen name="List" component={ListScreen}  options={{ headerShown: true,
         tabBarIcon: ({ color }) => (
-          <FontAwesomeIcon icon={faUserGraduate} color={color} size={20} />
+          <FontAwesomeIcon icon={faUsers} color={color} size={20} />
         ),}}/>
 
-      <Tab.Screen name="Github" component={WebScreen}  options={{ 
+      <Tab.Screen name="Map" component={MapScreen}  options={{ 
         tabBarIcon: ({ color }) => (
-          <FontAwesomeIcon icon={faGithub} color={color} size={20} />
+          <FontAwesomeIcon icon={faMap} color={color} size={20} />
         ),}}/>
 
       </Tab.Navigator>
